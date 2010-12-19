299badfe7984cd0377c60eb457b98acf51355f7f
Daniel Joo <dan@haerfest.us>
2010-12-19 14:30:53 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevHaerfestUs::Application.config.secret_token = 'fa18b9af7ad802765a9a4c37d1df9f1d61d8528778d70e5d5eacbeaf032c888174918c197fd1593cc986bba90efbcc1fa5c3250fb53855ce781f62ab88bff063'
