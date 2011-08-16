7683ec8535b52772e90fbd4f77807c13af183abc
daniel joo <askjoo@gmail.com>
2011-08-16 15:59:12 -0400

    moved title helper from pages_helper to top level applciation_helper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,12 +1,2 @@
 module PagesHelper
-
- # Return a title on a per-page basis.
-  def title
-    base_title = "HAERFEST"
-    if @title.nil?
-      base_title
-    else
-      "#{base_title} | #{@title}"
-    end
-  end
 end
