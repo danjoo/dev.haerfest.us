23a099a12d0f2a1a42ea5c3db24fb58c0534adcc
daniel joo <askjoo@gmail.com>
2011-08-18 02:10:51 -0400

    updated spec test to include new pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    @basetitle = 'HAERFEST'
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -13,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-      :content => "HAERFEST | Home")
+                                    :content => "#{@basetitle} | Home")
     end
 
     it "should not have a blank body" do
@@ -21,7 +24,58 @@ describe PagesController do
       response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
+  
+  describe "GET 'collections'" do
+    it "should be successful" do
+      get 'collections'
+      response.should be_success
+    end
 
+    it "should have the right title" do
+      get 'collections'
+      response.should have_selector("title",
+                                    :content => "#{@basetitle} | Collections")
+    end
+  end  
+  
+  describe "GET 'styles'" do
+    it "should be successful" do
+      get 'styles'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'styles'
+      response.should have_selector("title",
+                                    :content => "#{@basetitle} | Styles")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => "#{@basetitle} | About")
+    end
+  end
+
+  describe "GET 'stockists'" do
+    it "should be successful" do
+      get 'stockists'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'stockists'
+      response.should have_selector("title",
+                                    :content => "#{@basetitle} | Stockists")
+    end
+  end
 
   describe "GET 'contact'" do
     it "should be successful" do
@@ -32,19 +86,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-      :content => "HAERFEST | Contact")
-    end
-  end
-
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
-    it "should have the right title" do
-      get 'about'
-      response.should have_selector("title",
-      :content => "HAERFEST | About")
+                                    :content => "#{@basetitle} | Contact")
     end
   end
 
